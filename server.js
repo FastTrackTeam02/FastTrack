8eb782c69b42685098f61e9bbeaa59accf371f13
thanhbao0390 <46698469+thanhbao0390@users.noreply.github.com>
2019-01-16 11:09:00 +0700

    Update server.js

--- server.js
+++ server.js
@@ -12,14 +12,17 @@ app.use('/css', express.static('samples/css'))
 // route stop connection
 app.get('/db/stop', function (req, res) {
     connection.end()
+    res.send('Connection closed')
 })
 // route open connection
 app.get('/db/start', function (req, res) {
     connection.connect()
+    res.send('Connection open')
 })
 // route select
 app.get('/db/select', function (req, res) {
     select(req, res)
+    //express.static('samples/docs/select.html')
 })
 
 
@@ -40,10 +43,36 @@ connection.connect()
 //select database
 function select(req, res) {
 
-    connection.query('SELECT age from students', function (err, rows, fields) {
+    connection.query('SELECT * from students', function (err, rows, fields) {
         if (err) throw err
+        
+        const copy = [];
+        var isFirstLine = true;
+        copy.push("<h1>List data table</h1>")
+        copy.push("<table>")
 
-        res.send('The solution is: ' + rows[0].age)
+        rows.forEach(function(element) {
+
+            if (isFirstLine) {
+                copy.push("<tr>")
+                Object.keys(element).forEach(function(key) {
+                    copy.push("<th>" + key + "</th>")
+                });
+                copy.push("</tr>")
+                isFirstLine = false;
+            }
+            
+            copy.push("<tr>")
+            Object.keys(element).forEach(function(key) {
+                copy.push("<td>" + element[key] + "</td>")
+            });
+            copy.push("</tr>")
+
+        });
+
+        copy.push("</table>")
+
+        res.send('<!DOCTYPE html><html><head><title>Sample</title><style>table{border-collapse: collapse;}table, td, th {border: 1px solid black;}</style></head><body>' + copy.join("") + '</body></html>')
     })
 }
 
